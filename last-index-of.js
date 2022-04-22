26e1b06dc9a858ad9362898558b3ff56b347e64a
ttbinh139 <ttbinh139@gmail.com>
2022-04-22 21:23:44 +0000

    Add last-index-of.js

---
+++ last-index-of.js
@@ -0,0 +1,15 @@
+function lastIndexOf(array, value) {
+  let lastIndex = -1;
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === value){
+      lastIndex = i;
+    }
+  }
+  return lastIndex;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
