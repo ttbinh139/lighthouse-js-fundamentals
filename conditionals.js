d4a0b828cdb3d2921aeb686ce80d1904311708b2
ttbinh139 <ttbinh139@gmail.com>
2022-04-22 14:36:00 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -22,3 +22,13 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+
+
